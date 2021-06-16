8737f0a0d7a53f5a850aa76b4f21df3dfd873924
bavanya <kurrabchoudhry.cse18@itbhu.ac.in>
2021-06-17 00:16:06 +0530

    Update Main.java

--- sample_spark_project/src/main/java/Main.java
+++ sample_spark_project/src/main/java/Main.java
@@ -2,15 +2,42 @@ import java.io.IOException;
 import org.apache.spark.*;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
+        // Initialize spark session.
         SparkConf sparkConf = new SparkConf()
                 .setAppName("Example Spark App")
                 .setMaster("local[*]");  // Delete this line when submitting to a cluster
         JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
-        JavaRDD<String> stringJavaRDD = sparkContext.textFile("nationalparks.csv");
-        System.out.println("Number of lines in file = " + stringJavaRDD.count());
+
+        // Get the data to JavaRDD<String> from csv file.
+        JavaRDD<String> data = sparkContext.textFile("nationalparks.csv");
+
+        // Print the number of rows in the csv file.
+        System.out.println("Number of lines in file = " + data.count());
         System.out.println("Yayyy all good!!!");
+
+        // Get the national parks data into JavaRDD<Schema>.
+        JavaRDD<Schema> rdd_records = sparkContext.textFile("nationalparks.csv").map(
+                (Function<String, Schema>) line -> {
+                    // Here you can use JSON
+                    // Gson gson = new Gson();
+                    // gson.fromJson(line, Record.class);
+                    String[] fields = line.split(",");
+                    Schema sd = new Schema(fields[0], fields[1], Integer.parseInt(fields[2]), Float.parseFloat(fields[3]));
+                    return sd;
+                });
+
+        // Print the names of the national parks in the csv file.
+        rdd_records.foreach(item -> {
+            System.out.println("* " + item.name);
+        });
+
+        // TODO: Get only the rows with year_established >= 1900.
+
+
     }
 }
